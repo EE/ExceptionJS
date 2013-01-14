74dda15506d061534ecd91cc24d48177a9eea593
Piotr Antosik <mail@piotrantosik.com>
2013-01-14 14:06:43 +0100

    Remove jQuery required

--- Controller/ExceptionJSController.php
+++ Controller/ExceptionJSController.php
@@ -6,14 +6,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ExceptionJSController extends Controller
 {
-    public function catchErrorAction()
+    public function catchErrorAction(\Symfony\Component\HttpFoundation\Request $request)
     {
-        $logData = $this->getRequest()->get('jsError');
-        
-        $logger = $this->get('logger');
-        
-        $logger->err(json_encode($logData));
-        
-        return $this->render('EEExceptionJSBundle:ExceptionJS:catchError.json.twig', array('data' => json_encode($logData)));
+        if ($request->isXmlHttpRequest()) {
+            $logData = $request->getContent();
+
+            json_decode($logData);
+            if (JSON_ERROR_NONE !== json_last_error()) {
+                throw new \Symfony\Component\HttpKernel\Exception\BadRequestHttpException('No JSON content');
+            }
+
+            $logger = $this->get('logger');
+
+            $logger->err($logData);
+
+            return $this->render('EEExceptionJSBundle:ExceptionJS:catchError.json.twig', array('data' => json_encode($logData)));
+        }
+        else {
+            throw new \Symfony\Component\HttpKernel\Exception\BadRequestHttpException('No JSON request');
+        }
+
     }
+
 }
