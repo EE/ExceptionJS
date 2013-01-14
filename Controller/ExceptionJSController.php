458913cc7c0900f382af2fc30d4c3d54786ddc3d
Piotr Antosik <mail@piotrantosik.com>
2013-01-14 14:16:42 +0100

    Add new file to JavaScript logs

--- Controller/ExceptionJSController.php
+++ Controller/ExceptionJSController.php
@@ -3,27 +3,29 @@
 namespace EE\ExceptionJSBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception;
+use Symfony\Component\HttpFoundation\Request;
 
 class ExceptionJSController extends Controller
 {
-    public function catchErrorAction(\Symfony\Component\HttpFoundation\Request $request)
+    public function catchErrorAction(Request $request)
     {
         if ($request->isXmlHttpRequest()) {
             $logData = $request->getContent();
 
             json_decode($logData);
             if (JSON_ERROR_NONE !== json_last_error()) {
-                throw new \Symfony\Component\HttpKernel\Exception\BadRequestHttpException('No JSON content');
+                throw new BadRequestHttpException('No JSON content');
             }
 
             $logger = $this->get('logger');
-
+            $logger->pushHandler(new \Monolog\Handler\StreamHandler($this->get('kernel')->getRootDir().'/logs/'.$this->get('kernel')->getEnvironment().'.js.log', $logger::ERROR));
             $logger->err($logData);
 
             return $this->render('EEExceptionJSBundle:ExceptionJS:catchError.json.twig', array('data' => json_encode($logData)));
         }
         else {
-            throw new \Symfony\Component\HttpKernel\Exception\BadRequestHttpException('No JSON request');
+            throw new BadRequestHttpException('No JSON request');
         }
 
     }
